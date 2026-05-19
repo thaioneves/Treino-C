aab841de25021342b2618e511aee62aaa074f24b
thaioneves <thais.oneves2101@gmail.com>
2026-05-19 19:35:42 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const cliente1{
+    nome: 'Carlos Alberto';
+    cpf: 123.456.789.00,
+    idade: 24,
+    celular: 12345-6789
+    nickname: 'Kaka',
+    senha: 'kaka2104'
+}
+
+const cliente2{
+    nome: '';
+    cpf: ,
+    idade: ,
+    celular: 
+    nickname: '',
+    senha: ''
+}
