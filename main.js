c7ef28bc252537ba631a5868a73ea96b017c7c87
thaioneves <thais.oneves2101@gmail.com>
2026-05-21 20:29:58 -0300

    arrumação no simbolo do \n

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const cliente1 = {
     Senha: "kaka2404alb"
 };
 
+
 const cliente2 = {};
 cliente2.Nome = prompt("Qual o nome que deve ser registrado?")
 cliente2.CPF = prompt("Qual o CPF?")
@@ -17,18 +18,18 @@ cliente2.Senha = prompt("Qual a senha?")
 
 alert("Tudo certo, obrigado!")
 
-alert("Perfis do 1º cliente a frente./n"+
-    "Login do cliente:" + cliente1.Nome + "/n" +
-    "Idade do cliente:" + cliente1.Idade + "/n" +
-    "CPF do cliente:" + cliente1.CPF +"/n" +
-    "Telefone do cliente:" + cliente1.Telefone +"/n" +
-    "Nickname do clinte:" + cliente1.Nickname +"/n" +
-    "Senha do cliente:" + cliente1.Senha +"/n" +
+alert("Perfis do 1º cliente a frente.\n"+
+    "Login do cliente:" + cliente1.Nome + "\n" +
+    "Idade do cliente:" + cliente1.Idade + "\n" +
+    "CPF do cliente:" + cliente1.CPF +"\n" +
+    "Telefone do cliente:" + cliente1.Telefone +"\n" +
+    "Nickname do clinte:" + cliente1.Nickname +"\n" +
+    "Senha do cliente:" + cliente1.Senha +"\n" +
     
-    "/n" + "Perfis do 2º cliente a frente./n"+
-    "Login do cliente:" + cliente2.Nome +"/n" +
-    "Idade do cliente:" + cliente2.Idade +"/n" +
-    "CPF do cliente:" + cliente2.CPF +"/n" +
-    "Telefone do cliente:" + cliente2.Telefone +"/n" +
-    "Nickname do clinte:" + cliente2.Nickname +"/n" +
+    "\n" + "Perfis do 2º cliente a frente.\n"+
+    "Login do cliente:" + cliente2.Nome +"\n" +
+    "Idade do cliente:" + cliente2.Idade +"\n" +
+    "CPF do cliente:" + cliente2.CPF +"\n" +
+    "Telefone do cliente:" + cliente2.Telefone +"\n" +
+    "Nickname do clinte:" + cliente2.Nickname +"\n" +
     "Senha do cliente:" + cliente2.Senha)
